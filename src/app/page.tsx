dbfe34f22758c56ebb9dbe08c1f8396cafe507ff
abrazax56 <ahmadbenirusli@gmail.com>
2024-06-17 08:23:24 +0700

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-export default async function Form() {
+'use client';
+export default function Form() {
     const HandleSubmit = async (e: any) => {
+        'use server';
         e.preventDefault();
         const option = {
             method: 'POST',
