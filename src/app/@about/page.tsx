32e958351c9a64d0d6c7caab4c273d9532814026
Ahmad Beni Rusli <159234974+ahmdbnrsl@users.noreply.github.com>
2025-04-24 13:07:14 +0700

    Update page.tsx

--- src/app/@about/page.tsx
+++ src/app/@about/page.tsx
@@ -8,27 +8,13 @@ export default function About() {
             <div className='about-container'>
                 <div>
                     <p className='about-paragraph'>
-                        My name is{' '}
+                        I have a strong curiosity about new technologies and
+                        love exploring the latest advancements in the tech
+                        world. This curiosity fuels my ambition to become a{' '}
                         <span className='font-medium text-slate-200'>
-                            Ahmad Beni Rusli
+                            Fullstack Web Developer
                         </span>
-                        , and I come from the city of{' '}
-                        <span className='font-medium text-slate-200'>
-                            Cilacap
-                        </span>
-                        . Since I was young, I&#39;ve always had a passion for
-                        both{' '}
-                        <span className='font-medium text-slate-200'>
-                            coding
-                        </span>{' '}
-                        and{' '}
-                        <span className='font-medium text-slate-200'>
-                            painting
-                        </span>
-                        . These hobbies allow me to express my creativity and
-                        logical thinking in different ways. I find solace in the
-                        quiet moments spent debugging a complex code or
-                        perfecting a stroke on my canvas.
+                        .
                     </p>
                 </div>
                 <div>
@@ -43,17 +29,6 @@ export default function About() {
                         listening and observing, which helps me understand
                         things on a deeper level. This characteristic has served
                         me well in my academic pursuits and personal projects.
-                    </p>
-                </div>
-                <div>
-                    <p className='about-paragraph'>
-                        I have a strong curiosity about new technologies and
-                        love exploring the latest advancements in the tech
-                        world. This curiosity fuels my ambition to become a{' '}
-                        <span className='font-medium text-slate-200'>
-                            Fullstack Web Developer
-                        </span>
-                        .
                     </p>
                 </div>
             </div>
