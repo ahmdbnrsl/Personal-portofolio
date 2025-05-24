a1c7cdbc6101d4b083601ef821e539b81e92bf55
Ahmad Beni Rusli <159234974+ahmdbnrsl@users.noreply.github.com>
2025-05-24 08:40:23 +0700

    Update page.tsx

--- src/app/@footer/page.tsx
+++ src/app/@footer/page.tsx
@@ -7,7 +7,15 @@ export default function Footer() {
         >
             <div className='content-box'>
                 <div className=' tracking-wide text-sm sm:text-base lg:text-sm text-slate-500 w-fill max-w-[30ch] font-normal leading-normal'>
-                    <span>build with Next.js styling with Tailwind css{' '}</span>{' '}
+                    Follow My{' '}
+                    <Link
+                        href='https://whatsapp.com/channel/0029VbA6Dfl8KMqbps51g83J'
+                        target='_blank'
+                        className='text-slate-400 font-medium hover:text-sky-400 active:text-sky-400 cursor-pointer'
+                    >
+                        Whatsapp Channel
+                    </Link>{' '}
+                    build with Next.JS styling with Tailwind CSS{' '}
                     <span className='text-slate-400 font-medium'>© {new Date().getFullYear()}</span>{' '}
                     by{' '}
                     <span className='text-slate-400 font-medium'>
